be4fd4155cc0a3a21a50a7b5d1cd162ca16daf9c
Rock <zaqarea.ka0@gmail.com>
2021-08-15 15:15:57 +0300

    hello from qr org

--- app/src/main/java/com/example/StudentsQROrg/Login.java
+++ app/src/main/java/com/example/StudentsQROrg/Login.java
@@ -2,6 +2,7 @@ package com.example.StudentsQROrg;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -15,15 +16,20 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
@@ -65,82 +71,172 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    private void checkStudent(String email, String password){
-    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-    String url = "https://css4dev.com/QrStudents/login.php?email="+email+"&password="+password;
-    final JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, null, new com.android.volley.Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            Log.d("ffffffdfsfsa", url);
-            JSONArray jsonArray = null;
-            try {
-                jsonArray = response.getJSONArray("student");
-                Log.d("dsadaf45safsaf", jsonArray.length()+"");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            String id = null;
-            String name = null;
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject hit = null;
-                try {
-                    hit = jsonArray.getJSONObject(i);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    id = hit.getString("id");
-                    name = hit.getString("studentName");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (jsonArray.length() > 0){
-                SharedPreferences sharedPref = getSharedPreferences(FILE_CHECK_USER, Context.MODE_PRIVATE);
-                sharedPref.edit().putString("studentId", "true").apply();
-                SharedPreferences sharedPref2 = getSharedPreferences(FILE_CHECK_USER2, Context.MODE_PRIVATE);
-                sharedPref2.edit().putString("studentId2", id).apply();
-
-                Toast.makeText(Login.this, "welcome", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                intent.putExtra("studentId", id);
-                startActivity(intent);
-                finish();
-            }else {
-                Toast.makeText(Login.this, "كلمة السر او البريد الالكتروني غير صحيح", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }, new com.android.volley.Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            Log.d("dsadafsafsaf", error.getMessage());
-            Toast.makeText(Login.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        });
-
-    request.setRetryPolicy(new RetryPolicy() {
-        @Override
-        public int getCurrentTimeout() {
-            return 500000;
-        }
-
-        @Override
-        public int getCurrentRetryCount() {
-            return 3;
-        }
-
-        @Override
-        public void retry(VolleyError error) throws VolleyError {
-
-        }
-    });
-    requestQueue.add(request);
-}
+//    private void checkStudent(String email, String password){
+//    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+//    String url = "https://css4dev.com/QrCustomers/checkUser.php";
+//    final JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, null, new com.android.volley.Response.Listener<JSONObject>() {
+//        @Override
+//        public void onResponse(JSONObject response) {
+//            Log.d("ffffffdfsfsa", url);
+//            Log.d("ffffffdfsfsa2", response.toString());
+//            JSONArray jsonArray = null;
+//            try {
+//                jsonArray = response.getJSONArray("Users");
+//                Log.d("dsadaf45safsaf", jsonArray.length()+"");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            String id = null;
+//            String name = null;
+//            for (int i = 0; i < jsonArray.length(); i++) {
+//                JSONObject hit = null;
+//                try {
+//                    hit = jsonArray.getJSONObject(i);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    id = hit.getString("id");
+//                    name = hit.getString("name");
+//                    String age = hit.getString("age");
+//                    String phone = hit.getString("phone");
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (jsonArray.length() > 0){
+//                SharedPreferences sharedPref = getSharedPreferences(FILE_CHECK_USER, Context.MODE_PRIVATE);
+//                sharedPref.edit().putString("studentId", "true").apply();
+//                SharedPreferences sharedPref2 = getSharedPreferences(FILE_CHECK_USER2, Context.MODE_PRIVATE);
+//                sharedPref2.edit().putString("studentId2", id).apply();
+//
+//                Toast.makeText(Login.this, "welcome", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(Login.this, MainActivity.class);
+//                intent.putExtra("studentId", id);
+//                startActivity(intent);
+//                finish();
+//            }else {
+//                Toast.makeText(Login.this, "كلمة السر او البريد الالكتروني غير صحيح", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }, new com.android.volley.Response.ErrorListener() {
+//        @Override
+//        public void onErrorResponse(VolleyError error) {
+//            Log.d("dsadafsafsaf", error.getMessage());
+//            Toast.makeText(Login.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+//        }
+//        })
+//    {
+//        @Override
+//        protected Map<String, String> getParams() {
+//            Map<String, String> params = new HashMap<>();
+//            params.put("email", email);
+//            params.put("password",password);
+//            return params;
+//        }
+//    };
+//
+//    request.setRetryPolicy(new RetryPolicy() {
+//        @Override
+//        public int getCurrentTimeout() {
+//            return 500000;
+//        }
+//
+//        @Override
+//        public int getCurrentRetryCount() {
+//            return 3;
+//        }
+//
+//        @Override
+//        public void retry(VolleyError error) throws VolleyError {
+//
+//        }
+//    });
+//    requestQueue.add(request);
+//}
 
     @Override
     protected void onStart() {
         super.onStart();
 
+    }
+
+    private void checkStudent(String email, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.getCache().clear();
+        String url = "https://css4dev.com/QrCustomers/checkUser.php";
+        StringRequest strRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("res",response);
+                try {
+                    JSONObject res = new JSONObject(response);
+                    JSONArray jsonArray = null;
+                    try {
+                        jsonArray = res.getJSONArray("Users");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String id = null;
+                    String name = null;
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject hit = null;
+                        try {
+                            hit = jsonArray.getJSONObject(i);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            id = hit.getString("id");
+                            name = hit.getString("name");
+                            String age = hit.getString("age");
+                            String phone = hit.getString("phone");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (jsonArray.length() > 0){
+                        SharedPreferences sharedPref = getSharedPreferences(FILE_CHECK_USER, Context.MODE_PRIVATE);
+                        sharedPref.edit().putString("studentId", "true").apply();
+                        SharedPreferences sharedPref2 = getSharedPreferences(FILE_CHECK_USER2, Context.MODE_PRIVATE);
+                        sharedPref2.edit().putString("studentId2", id).apply();
+
+                        Toast.makeText(Login.this, "welcome", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        intent.putExtra("studentId", id);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Toast.makeText(Login.this, "كلمة السر او البريد الالكتروني غير صحيح", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(Login.this, "error.getMessage()", Toast.LENGTH_SHORT).show();
+//                            progressBar.setVisibility(View.GONE);
+//                            error.printStackTrace();
+                    }
+                })
+        {
+        @Override
+        protected Map<String, String> getParams() {
+            Map<String, String> params = new HashMap<>();
+            params.put("email", email);
+            params.put("password",password);
+            return params;
+        }
+    };
+
+        strRequest.setShouldCache(false);
+        requestQueue.add(strRequest);
     }
 }
